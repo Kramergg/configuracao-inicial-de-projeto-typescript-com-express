94dfeaf8a5468653d3ef7ad2f7e98c036bc54392
Gabriel Kramer <kramermota55@gmail.com>
2024-08-01 10:16:28 -0400

    configuracao completa para começar projeto v1.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("heelo world");
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
